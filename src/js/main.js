fda4c91189739c58f4e78d0b1c4480b06a46821c
Scott Vrable <scott@scottvrable.com>
2014-08-30 23:46:38 -0400

    Fix queuing error message problem for real this time
    Problem was not fixed when previously stated in Git
    Add .stop() to removeError() function

--- src/js/main.js
+++ src/js/main.js
@@ -85,7 +85,7 @@ function removeSpaces(inputObj) {
 var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function hideErrors() {
-	$('.errorBox').fadeOut(200);
+	$('.errorBox').stop().fadeOut(200);
 }
 
 function validate(str) {
