be655f4829607c21da8b6b314c09fc318411c8ca
Scott Vrable <scott@scottvrable.com>
2014-08-31 12:25:49 -0400

    Reorganize main.js in more linear fashion
    Make main.js more readable to read better from top to bottom and
    make refactoring the code easier

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@
 // remove no JavaScript warning
 $('#noJavaScript').remove();
 
-//grabbing variables
+// ========================= begin grabbing variables //
+
+// jQuery variables
 var heading$           = $('h1'),
     headerHolder$      = $('#headerHolder'),
     formHolder$        = $('#formHolder'),
@@ -12,6 +14,7 @@ var heading$           = $('h1'),
     textField$         = $('#textField'),
     inputText$         = $('#inputText'),
     divCheck$          = $('#divCheck'),
+    errorBoxes$        = $('.errorBox'),
     errorEmpty$        = $('#errorEmpty'),
     errorBad$          = $('#errorBad'),
     submitButton$      = $('#submitButton'),
@@ -20,93 +23,20 @@ var heading$           = $('h1'),
     theCode$           = $('#theCode'),
     theX$              = $(preloadImage('img/check-x.png'));
 
-// ========================= begin setting up page //
+// regular JavaScript variables
+var isDivChecked       = false,
+    // email validation regular expression
+    emailRegex         = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    // regular expressions to clean HTML for output
+    reg1               = /'/g,
+    reg2               = /"/g,
+    reg3               = /</g,
+    reg4               = />/g;
 
-// set up initial animation
-heading$.css('top', '-50%');
-headerHolder$.css('top', '50%').hide();
-textLabel$.css('opacity', 0);
-inputText$.css('opacity', 0);
-formHolder$.hide();
-textField$.hide();
+// array variables
+var letters            = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//x checkbox
-theX$.attr({'alt': 'X', 'class': 'theX', 'id': 'theX'});
-var isDivChecked = false;
-
-// ========================= end setting up page //
-
-function preloadImage(src, func) {
-	var img = new Image();
-	img.src = src;
-	if(func) {
-		img.onload = func;
-	}
-	return img;
-}
-
-function animateLogo() {
-	headerHolder$.fadeIn(1000, function() {
-		heading$.delay(500).animate({top: "+=50%"}, 1000);
-		headerHolder$.delay(500).animate({top: "-=50%"}, 1000, function() {
-			formHolder$.fadeIn(500);
-		});
-	});
-}
-
-preloadImage('img/magic-spambot-preventing-email-link-generator.jpg', animateLogo);
-
-
-// Clear out default values on focus
-function clearDefaultVal(inputObj, removeClass) {
-	if(inputObj.val() === inputObj.prop('defaultValue')) {
-		inputObj.val('');
-	}
-	if(removeClass) {
-		inputObj.removeClass(removeClass);
-	}
-}
-// restore default values on blur if left blank
-function restoreDefaultVal(inputObj, addClass) {
-	if(inputObj.val() === '') {
-		inputObj.val(inputObj.prop('defaultValue'));
-	}
-	if(addClass) {
-		inputObj.addClass(addClass);
-	}
-}
-// remove spaces from email input as user is typing
-function removeSpaces(inputObj) {
-	return inputObj.val(inputObj.val().replace(/\s/, ''));
-}
-
-// ========================= begin validation //
-
-var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-function hideErrors() {
-	$('.errorBox').stop().fadeOut(200);
-}
-
-function validate(str) {
-	if(str === inputEmail$.prop('defaultValue')) {
-		errorEmpty$.stop().fadeIn(200);
-	} else if(!emailRegex.test(str)) {
-		errorBad$.stop().fadeIn(200);
-	} else {
-		makeCode(str);
-	}
-}
-
-// ========================= end validation // 
-
-// ========================= making the code // 
-
-// general regEx to make code printable
-var reg1 = /'/g, reg2 = /"/g, reg3 = /</g, reg4 = />/g;
-
-var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+// object variables
 var strObj = {
 	originalEmail: null,
 	safeText: null,
@@ -117,27 +47,136 @@ var strObj = {
 	dateID: null
 };
 
-function showOutput(outputString) {
-	theCode$.html(outputString);
-	mask$.fadeIn(400);
-	if(theCode$.prop('scrollHeight')) {
-		theCode$.css('height', theCode$.prop('scrollHeight') - 40);
+// ========================= finish grabbing variables //
+
+// ========================= begin setting up page //
+
+// setting up start point for logo animation
+heading$.css('top', '-50%');
+headerHolder$.css('top', '50%').hide();
+textLabel$.css('opacity', 0);
+inputText$.css('opacity', 0);
+formHolder$.hide();
+textField$.hide();
+
+// preloading, then triggering animation of logo
+preloadImage('img/magic-spambot-preventing-email-link-generator.jpg', animateLogo);
+
+// x checkbox, graphic is preloaded above
+theX$.attr({'alt': 'X', 'class': 'theX', 'id': 'theX'});
+
+// === functions for immediate use
+
+// preload graphics function
+function preloadImage(src, func) {
+	var img = new Image();
+	img.src = src;
+	if(func) {
+		img.onload = func;
+	}
+	return img;
+}
+
+// animate logo after being preloaded
+function animateLogo() {
+	headerHolder$.fadeIn(1000, function() {
+		heading$.delay(500).animate({top: "+=50%"}, 1000);
+		headerHolder$.delay(500).animate({top: "-=50%"}, 1000, function() {
+			formHolder$.fadeIn(500);
+		});
+	});
+}
+
+// ========================= end setting up page //
+
+// ========================= begin input field events //
+
+// Clear out default values on focus
+function clearDefaultVal(inputObj, removeClass) {
+	if(inputObj.val() === inputObj.prop('defaultValue')) {
+		inputObj.val('');
+	}
+	if(removeClass) {
+		inputObj.removeClass(removeClass);
+	}
+}
+
+// restore default values on blur if left blank
+function restoreDefaultVal(inputObj, addClass) {
+	if(inputObj.val() === '') {
+		inputObj.val(inputObj.prop('defaultValue'));
+	}
+	if(addClass) {
+		inputObj.addClass(addClass);
+	}
+}
+
+// remove spaces from email input as user is typing
+function removeSpaces(inputObj) {
+	return inputObj.val(inputObj.val().replace(/\s/, ''));
+}
+
+// ========================= end input field events //
+
+// ========================= begin validation //
+
+// main validation
+function validate(str) {
+	if(str === inputEmail$.prop('defaultValue')) {
+		errorEmpty$.stop().fadeIn(200);
+	} else if(!emailRegex.test(str)) {
+		errorBad$.stop().fadeIn(200);
 	} else {
-		theCode$.css('height', 350);
+		makeCode(str);
 	}
 }
 
-function makeDateID() {
-	strObj.dateID = letters[Math.floor(Math.random() * letters.length)] + new Date().getTime();
+// clear validation
+function hideErrors() {
+	$('.errorBox').stop().fadeOut(200);
 }
 
-function chopEmail() {
-	var choppedArray = [];
-	for(var i = 0, count = strObj.num; i < count; i++) {
-		choppedArray[i] = strObj.originalEmail.slice((i * 3), (i * 3 + 3));
+// ========================= end validation // 
+
+// ========================= begin making the code // 
+
+
+function makeCode(str) {
+	var theText;
+	str = str.toLowerCase();
+	strObj.originalEmail = str;
+	if(isDivChecked === true && inputText$.val() !== inputText$.prop('defaultValue')) {
+		theText = inputText$.val();
+		theText = theText.replace(reg1, '&#39;').replace(reg2, '&#34;').replace(reg3, '&#60;').replace(reg4, '&#62;');
+		strObj.customText = theText;
+	} else {
+		strObj.customText = '';
 	}
-	strObj.choppedArray = choppedArray;
-	makeDateID();
+	makeSafeText();
+	showOutput(pieceTogether());
+}
+
+function makeSafeText() {
+	var reAt = /@/g;
+	var reDot = /\./g;
+	var newString = strObj.originalEmail.replace(reAt, ' at ');
+	newString = newString.replace(reDot, ' dot ');
+	strObj.safeText = newString;
+	howManyPieces();
+}
+
+function howManyPieces() {
+	strObj.num = Math.ceil(strObj.originalEmail.length/3);
+	makeVars(strObj.num);
+}
+
+function makeVars() {
+	var varArray = [], num = strObj.num;
+	for(var i = 0, count = num; i < count; i++) {
+		varArray[i] = makeVariable();
+	}
+	strObj.varArray = varArray;
+	chopEmail();
 }
 
 function makeVariable() {
@@ -159,100 +198,32 @@ function makeVariable() {
 	return varStr;
 }
 
-function makeVars() {
-	var varArray = [], num = strObj.num;
-	for(var i = 0, count = num; i < count; i++) {
-		varArray[i] = makeVariable();
+function chopEmail() {
+	var choppedArray = [];
+	for(var i = 0, count = strObj.num; i < count; i++) {
+		choppedArray[i] = strObj.originalEmail.slice((i * 3), (i * 3 + 3));
 	}
-	strObj.varArray = varArray;
-	chopEmail();
+	strObj.choppedArray = choppedArray;
+	makeDateID();
 }
 
-function howManyPieces() {
-	strObj.num = Math.ceil(strObj.originalEmail.length/3);
-	makeVars(strObj.num);
+function makeDateID() {
+	strObj.dateID = letters[Math.floor(Math.random() * letters.length)] + new Date().getTime();
 }
 
-function makeSafeText() {
-	var reAt = /@/g;
-	var reDot = /\./g;
-	var newString = strObj.originalEmail.replace(reAt, ' at ');
-	newString = newString.replace(reDot, ' dot ');
-	strObj.safeText = newString;
-	howManyPieces();
-}
-
-function makeCode(str) {
-	var theText;
-	str = str.toLowerCase();
-	strObj.originalEmail = str;
-	if(isDivChecked === true && inputText$.val() !== inputText$.prop('defaultValue')) {
-		theText = inputText$.val();
-		theText = theText.replace(reg1, '&#39;').replace(reg2, '&#34;').replace(reg3, '&#60;').replace(reg4, '&#62;');
-		strObj.customText = theText;
+function showOutput(outputString) {
+	theCode$.html(outputString);
+	mask$.fadeIn(400);
+	if(theCode$.prop('scrollHeight')) {
+		theCode$.css('height', theCode$.prop('scrollHeight') - 40);
 	} else {
-		strObj.customText = '';
+		theCode$.css('height', 350);
 	}
-	makeSafeText();
-	showOutput(pieceTogether());
 }
 
 // ========================= end making the code // 
 
-inputEmail$.focus(function() {
-	hideErrors();
-	clearDefaultVal($(this), 'mute');
-});
-inputEmail$.keyup(function() {
-	removeSpaces($(this));
-});
-inputEmail$.blur(function() {
-	restoreDefaultVal($(this), 'mute');
-});
-inputText$.focus(function() {
-	hideErrors();
-	clearDefaultVal($(this), 'mute');
-});
-inputText$.blur(function() {
-	restoreDefaultVal($(this), 'mute');
-});
-divCheck$.click(function() {
-	hideErrors();
-	if(!isDivChecked) {
-		$(this).append(theX$);
-		isDivChecked = true;
-		textField$.stop().slideDown(400, function() {
-			textLabel$.stop().animate({'opacity': 1}, 200);
-			inputText$.stop().animate({'opacity': 1}, 200);
-		});
-	} else {
-		theX$.remove();
-		isDivChecked = false;
-		textField$.stop().slideUp(200, function() {
-			textLabel$.stop().css({'opacity': 0});
-			inputText$.stop().css({'opacity': 0});
-		});
-	}
-});
-errorEmpty$.click(function() {
-	hideErrors();
-});
-errorBad$.click(function() {
-	hideErrors();
-});
-submitButton$.stop().click(function(e) {
-	e.preventDefault();
-	hideErrors();
-	validate(inputEmail$.val());
-});
-mask$.click(function() {
-	$(this).fadeOut();
-});
-output$.click(function(e) {
-	e.stopPropagation();
-});
-
-/* =========================================== PIECING THE STRING TOGETHER */
+// ========================= piecing string together // 
 
 function pieceTogether() {
 	var outputString = '';
@@ -302,5 +273,74 @@ function pieceTogether() {
 	outputString = outputString.replace(reg1, '&#39;').replace(reg2, '&#34;').replace(reg3, '&#60;').replace(reg4, '&#62;');
 	return outputString;
 }
+
+// ========================= end piecing string together //
+
+// ========================= begin attaching events to inputs // 
+
+// inputEmail field events
+inputEmail$.focus(function() {
+	hideErrors();
+	clearDefaultVal($(this), 'mute');
+});
+inputEmail$.keyup(function() {
+	removeSpaces($(this));
+});
+inputEmail$.blur(function() {
+	restoreDefaultVal($(this), 'mute');
+});
+
+// inputText field events
+inputText$.focus(function() {
+	hideErrors();
+	clearDefaultVal($(this), 'mute');
+});
+inputText$.blur(function() {
+	restoreDefaultVal($(this), 'mute');
+});
+
+// divCheck events
+divCheck$.click(function() {
+	hideErrors();
+	if(!isDivChecked) {
+		$(this).append(theX$);
+		isDivChecked = true;
+		textField$.stop().slideDown(400, function() {
+			textLabel$.stop().animate({'opacity': 1}, 200);
+			inputText$.stop().animate({'opacity': 1}, 200);
+		});
+	} else {
+		theX$.remove();
+		isDivChecked = false;
+		textField$.stop().slideUp(200, function() {
+			textLabel$.stop().css({'opacity': 0});
+			inputText$.stop().css({'opacity': 0});
+		});
+	}
+});
+
+//error message events
+errorBoxes$.click(function() {
+	hideErrors();
+});
+
+// submitButton events
+submitButton$.stop().click(function(e) {
+	e.preventDefault();
+	hideErrors();
+	validate(inputEmail$.val());
+});
+
+// mask events
+mask$.click(function() {
+	$(this).fadeOut();
+});
+
+// output events
+output$.click(function(e) {
+	e.stopPropagation();
+});
+
+// ========================= end attaching events to inputs //
 
 })(jQuery);
